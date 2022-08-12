4b53b9eafec93c64212f05d6f88a8b7638b44091
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-12 19:15:10 +0300

    Начало написания тестов Mock - 2 часть

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -3,7 +3,9 @@ package ru.netology.sender;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -12,8 +14,7 @@ import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
 import ru.netology.i18n.LocalizationService;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 /**
@@ -61,5 +62,30 @@ class MessageSenderImplTest {
         MatcherAssert.assertThat(result, Matchers.equalTo(expected));
 
 
+    }
+
+
+    @Test
+    void test_send_message_if_ip_usa() {
+        List<Country> countryUsa = Arrays.asList(Country.USA, Country.BRAZIL, Country.GERMANY);
+
+        final Location newYork = new Location("New York", Country.USA, null, 0);
+        final String ip = "96.44.183.149";
+
+        GeoService geoService = Mockito.mock(GeoService.class);
+        Mockito.when(geoService.byIp(Mockito.anyString()))
+                .thenReturn(newYork);
+
+        LocalizationService localizationService = Mockito.mock(LocalizationService.class);
+        Mockito.when(localizationService.locale(countryUsa.get(0)))
+                .thenReturn("Welcome");
+
+        Map<String, String> headers = new HashMap<String, String>();
+        messageSender = new MessageSenderImpl(geoService, localizationService);
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, ip);
+        String result = messageSender.send(headers);
+        String expected = "Welcome";
+
+        MatcherAssert.assertThat(result, Matchers.equalTo(expected));
     }
 }
